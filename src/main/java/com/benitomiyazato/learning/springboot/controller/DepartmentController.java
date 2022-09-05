f9939c750c8acd484b2bfc03ccb3a8d79ffd02e1
bemiyaz <benito.miya172@gmail.com>
2022-09-05 14:38:02 -0300

    get request: findAll

--- src/main/java/com/benitomiyazato/learning/springboot/controller/DepartmentController.java
+++ src/main/java/com/benitomiyazato/learning/springboot/controller/DepartmentController.java
@@ -3,9 +3,12 @@ package com.benitomiyazato.learning.springboot.controller;
 import com.benitomiyazato.learning.springboot.entity.Department;
 import com.benitomiyazato.learning.springboot.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -16,5 +19,10 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
     }
 }
--- src/main/java/com/benitomiyazato/learning/springboot/service/DepartmentService.java
+++ src/main/java/com/benitomiyazato/learning/springboot/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.benitomiyazato.learning.springboot.service;
 
 import com.benitomiyazato.learning.springboot.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
 }
