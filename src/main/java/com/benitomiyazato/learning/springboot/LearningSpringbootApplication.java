7b05096f41af393a00fdf23424c4a267d936e76b
bemiyaz <benito.miya172@gmail.com>
2022-09-05 12:35:26 -0300

    HelloControler

--- src/main/java/com/benitomiyazato/learning/springboot/LearningSpringbootApplication.java
+++ src/main/java/com/benitomiyazato/learning/springboot/LearningSpringbootApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearningSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningSpringbootApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringbootApplication.class, args);
+    }
 }
